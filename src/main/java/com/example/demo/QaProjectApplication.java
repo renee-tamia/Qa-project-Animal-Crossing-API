4214e9f8912551a4f0eb73c69535a9f6a1673033
renee-tamia <renz246@hotmail.co.uk>
2021-12-01 10:43:56 +0000

    first commit

---
+++ src/main/java/com/example/demo/QaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaProjectApplication.class, args);
+	}
+
+}
