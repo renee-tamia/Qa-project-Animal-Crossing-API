5174824ea4aaf78672ac6773df25cff01014af9b
renee-tamia <renz246@hotmail.co.uk>
2021-12-01 10:54:03 +0000

    model class finished

--- src/main/java/com/example/demo/model/AcCharacters.java
+++ src/main/java/com/example/demo/model/AcCharacters.java
@@ -18,4 +18,91 @@ public class AcCharacters {
 	private String hobbies;
 	private String catchphrase;
 	
+	public AcCharacters() {
+		
+	}
+	
+	
+	public AcCharacters(Integer id, String name, String species, String birthday, String personality,
+			String hobbies, String catchphrase) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.species = species;
+		this.birthday = birthday;
+		this.personality = personality;
+		this.hobbies = hobbies;
+		this.catchphrase = catchphrase;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSpecies() {
+		return species;
+	}
+
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+
+	public String getPersonality() {
+		return personality;
+	}
+
+
+	public void setPersonality(String personality) {
+		this.personality = personality;
+	}
+
+
+	public String getHobbies() {
+		return hobbies;
+	}
+
+
+	public void setHobbies(String hobbies) {
+		this.hobbies = hobbies;
+	}
+
+
+	public String getCatchphrase() {
+		return catchphrase;
+	}
+
+
+	public void setCatchphrase(String catchphrase) {
+		this.catchphrase = catchphrase;
+	}
+	
 }
