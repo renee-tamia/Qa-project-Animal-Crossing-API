40adf797beeed0f242fc846cf794561ba9645cbe
renee-tamia <renz246@hotmail.co.uk>
2021-12-01 12:54:03 +0000

    custom query in repo

--- src/main/java/com/example/demo/repo/AcRepo.java
+++ src/main/java/com/example/demo/repo/AcRepo.java
@@ -1,5 +1,8 @@
 package com.example.demo.repo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,6 @@ import com.example.demo.model.AcCharacters;
 @Repository
 public interface AcRepo extends JpaRepository<AcCharacters, Integer> {
 
+	Optional<List<AcCharacters>> findByName(String name);
+	
 }
