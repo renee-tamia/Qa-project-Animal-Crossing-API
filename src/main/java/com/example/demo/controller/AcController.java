40ac591d90d3680986bd927904d1d1eac6d1780e
renee-tamia <renz246@hotmail.co.uk>
2021-12-01 12:34:47 +0000

    created create function in controller

--- src/main/java/com/example/demo/controller/AcController.java
+++ src/main/java/com/example/demo/controller/AcController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,11 @@ public class AcController {
 		return response;
 	}
 
+	@PostMapping("/create") //  PASSED TEST
+	public ResponseEntity<AcCharacters> createCharacter(@RequestBody AcCharacters character) {
+		ResponseEntity<AcCharacters> response = new ResponseEntity<AcCharacters>(
+				this.service.createCharacter(character), HttpStatus.CREATED);
+		return response;
+	}
 	
 }
--- src/main/java/com/example/demo/service/AcService.java
+++ src/main/java/com/example/demo/service/AcService.java
@@ -26,4 +26,6 @@ public class AcService {
 		return foundCharacter;
 	}
 
+	
+	
 }
